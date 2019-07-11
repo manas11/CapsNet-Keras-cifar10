768a345d68f0624311af33b0f15fd3f62b2036f1
zhongsb <z20107824@163.com>
2019-07-12 07:43:13 +0800

    formate kifar-10 to 28,28

--- capsulenet.py
+++ capsulenet.py
@@ -205,6 +205,10 @@ def load_CIFAR():
     from keras.datasets import cifar10
     # The data, split between train and test sets:
     (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    y_train = to_categorical(y_train.astype('float32'))
+    y_test = to_categorical(y_test.astype('float32'))
     return (x_train, y_train), (x_test, y_test)
 
 if __name__ == "__main__":
